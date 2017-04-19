c15f3bffd5a0fac421cae9548dc05b9dc7645a54
010011100010 <ll3305@nyu.edu>
2017-04-19 14:05:29 -0400

    Vector math

---
+++ Math/Assets/VectorMath.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorMath : MonoBehaviour {
+	public Vector3 startPoint = new Vector3 (0, 0, 0); //this is a constructor!
+	public Vector3 vec1 = new Vector3 (10, 0, 0);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawRay (startPoint, vec1, Color.yellow); //Draw ray is a static function in a class
+		Debug.Log("Vec1 Mag: "+vec1.magnitude);
+		Debug.Log ("Vec1 Normalized: " + Vector3.Normalize (vec1));
+
+		Debug.Log ("Dot :"+Vector3.Dot (Vector3.right, Vector3.down));
+
+		Debug.Log ("Facing :" + transform.forward);
+		//if (Vector3.Dot(transform.forward, dir, Color.cyan);
+	}
+}
---
+++ Math/Assets/moveTowardTarget.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTowardTarget : MonoBehaviour {
+	public Transform target;
+	public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = target.position-transform.position;
+		Debug.DrawRay (transform.position, dir, Color.magenta);
+
+		dir	= new Vector3 (dir.x / dir.magnitude, dir.y / dir.magnitude, dir.z / dir.magnitude);
+		//Normalize will normalize the vector, change the value inside the vector; normalized will return a normalized vector but not change the value in the original one
+
+		Debug.Log ("normalized dir :" + dir.magnitude);
+
+
+		Debug.Log ("Facing :" + transform.forward);
+		if (Vector3.Dot(transform.forward, dir)>0){
+			Debug.DrawRay (transform.position, dir, Color.cyan);
+			transform.Translate (dir*speed*Time.deltaTime);
+		}
+	}
+}
